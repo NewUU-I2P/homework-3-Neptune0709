f28342c8d7f900a597c216070e327f932e13ca05
Neptune0709 <147371747+Neptune0709@users.noreply.github.com>
2023-11-12 22:58:50 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,24 +1,26 @@
 #include <string>
 std::string problemSolution3(float height, char S) {
     string result;
-    if (s==77){
-        if (height<1.70){
-            result="Short";
-        } else if (height>=1.70&&height<1.85){
-            result="Normal";
-        } else{
-            result="Tall";
-        }
-    } else if (S==70){
-        if (height<1.60){
-            result="Short";
-        } else if (height>=1.60&&height<1.75){
-            result="Normal";
-        } else{
-            result="Tall";
-        }
-    } else {
-        result="Invalid input";
+    // write your code here
+    switch (S){
+        case 77:
+            if (height<1.70){   
+                result="Short";
+            } else if (height>=1.70&&height<1.85){
+                result="Normal";
+            } else {
+                result = "Tall";
+            }
+        case 70:
+            if (height<1.60){
+                result="Short";
+            } else if (height>=1.60&&height<1.75){
+                result="Normal";
+            } else{
+                result="Tall";
+            }
+        default :
+            result="Invalid input";
     }
     return result;
 }

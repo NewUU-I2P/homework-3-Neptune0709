f26bf2b93c2546f4b806951fb34b97bcddcc3477
Neptune0709 <147371747+Neptune0709@users.noreply.github.com>
2023-11-12 22:42:29 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,32 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
+   switch (operation){
+        case '+':
+            result=x+y;
+            break;
+        case '-':
+            result=x-y;
+            break;
+        case '*':
+            result=x*y;
+            break;
+        case '/':
+            result=x/y;
+            break;
+    }
    return result;
+}
+#include "iostream"
+using namespace std;
+int main (){
+    float x;
+    float y;
+    char operation;
+    cout<<"Enter two integers:"<<endl;
+    cin>>x>>y;
+    cout<<"Choose the operation (+,-,*,/):"<<endl;
+    cin>>operation;
+    cout<<problemSolution5(x, y, operation);
+
 }
